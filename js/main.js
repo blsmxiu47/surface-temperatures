f568bb6355c521b2d2dbea7f61a1de6faed9ab74
Wes Warriner <fridgehistory@gmail.com>
2022-01-15 12:33:22 -0800

    add initial MapView with ArcGIS API for JS

---
+++ js/main.js
@@ -0,0 +1,15 @@
+require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,Map, MapView) {
+      
+});
+esriConfig.apiKey = process.env.ARCGIS_API_KEY;
+
+const map = new Map({
+    basemap: "arcgis-topographic" // Basemap layer service
+});
+
+const view = new MapView({
+    map: map,
+    center: [-118.805, 34.027], // Longitude, latitude
+    zoom: 13, // Zoom level
+    container: "viewDiv" // Div element
+});
